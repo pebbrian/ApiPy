ca8afd687226d0c34d3248c2e74479dfafe73862
besevic <ivan.besevic_fr@yahoo.com>
2019-02-19 14:26:25 +0100

    Update the package

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="PyApiManager",
-    version="0.0.2",
+    version="0.0.3",
     author="Brian Pierre-Emmanuel",
     author_email="pebrian@outlook.fr",
     description="This is a usefull api manager.",

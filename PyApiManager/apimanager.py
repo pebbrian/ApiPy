9e4ae623eac2be610f3d7f3ae86e41bd9fdcc717
besevic <ivan.besevic_fr@yahoo.com>
2019-02-19 16:29:22 +0100

    Update the package

--- PyApiManager/apimanager.py
+++ PyApiManager/apimanager.py
@@ -7,7 +7,7 @@ class API_Manager:
         ver: 190114
     """
 
-    def __init__(self, def_path: str):
+    def __init__(self, def_path: list):
         """
             Constructeur : charge et stocke la définition de l'API si elle est correcte.
             Lance une erreur spécifique le cas contraire.
@@ -16,21 +16,23 @@ class API_Manager:
 
         for path in def_path:
             with open(path) as file:
-                API_def = json.load(file)
-                self.check_def(API_def)
-                self.API_list.append(API_def)
+                api_def = json.load(file)
+                self.check_def(api_def)
+                self.API_list.append(api_def)
 
         self.selected_API = None
 
-    def check_def(self, API_def):
+    @staticmethod
+    def check_def(api_def):
         """
             Vérifie que la définition soit correcte.
             Lance une erreur spécifique le cas contraire.
         """
-        if not 'url' or not 'requests' in API_def:
-            raise Exception('"url" and/or "requests" key missing in API definition')
+        if not 'url' or 'requests' not in api_def:
+            raise ValueError('"url" and/or "requests" key missing in API definition')
 
-    def check_required_params(self, params: dict, payload: dict):
+    @staticmethod
+    def check_required_params(params: dict, payload: dict):
         """
             Vérifie que la paramètres requis soient bien présents dans la charge utile.
             Lance une erreur spécifique le cas contraire.
@@ -38,19 +40,21 @@ class API_Manager:
         for par in params:
             if 'required' in params[par]:
                 if params[par]['required']:
-                    if not par in payload:
-                        raise Exception('Required argument not provided : ' + par)
+                    if par not in payload:
+                        raise ValueError('Required argument not provided : ' + par)
 
-    def check_unknow_params(self, params: dict, payload: dict):
+    @staticmethod
+    def check_unknow_params(params: dict, payload: dict):
         """
             Vérifie que les paramètres soient présents dans la définition.
             Lance une erreur spécifique le cas contraire.
         """
         for par in payload:
-            if not par in params:
-                raise Exception('Unknow parameter : ' + par)
+            if par not in params:
+                raise ValueError('Unknow parameter : ' + par)
 
-    def check_params_values(self, params: dict, payload: dict):
+    @staticmethod
+    def check_params_values(params: dict, payload: dict):
         """
             Vérifie que les valeurs des paramètres de la charge utile possèdent bien des valeurs acceptées.
             Lance une erreur spécifique le cas contraire.
@@ -59,9 +63,10 @@ class API_Manager:
             if par in params:
                 if 'values' in par:
                     if payload[par] not in par['values']:
-                        raise Exception('Unknow parameter : ' + par)
+                        raise ValueError('Unknow parameter : ' + par)
 
-    def add_default_params(self, params: dict, payload: dict):
+    @staticmethod
+    def add_default_params(params: dict, payload: dict):
         """
             Ajoute les paramètres par défaut à la charge utile et retourne le résultat.
         """
@@ -98,8 +103,8 @@ class API_Manager:
             Vérifie que la requête soit présente dans la définition.
             Lance une erreur spécifique le cas échéant.
         """
-        if not request in self.selected_API['requests']:
-            raise Exception('Unknow request : ' + request)
+        if request not in self.selected_API['requests']:
+            raise ValueError('Unknow request : ' + request)
 
     def complete_payload(self, request: str, payload: dict):
         """
@@ -107,20 +112,26 @@ class API_Manager:
         """
         return self.add_default_params(self.get_params(request), payload)
 
-    def check_params(self, params: dict, payload: dict = {}):
+    def check_params(self, params: dict, payload: dict = None):
         """
             Vérifie que la requête soit correcte.
             Lance une erreur spécifique le cas échéant.
         """
+        if payload is None:
+            payload = {}
+
         self.check_unknow_params(params, payload)  # Validation : Tous les paramètres sont reconnus
         self.check_required_params(params, payload)  # Validation : Tous les paramètres requis sont présents
         self.check_params_values(params, payload)  # Validation : Les valeurs spécifiées pour les paramètres sont acceptées
 
-    def get(self, request: str, payload: dict = {}):
+    def get(self, request: str, payload: dict = None):
         """
             Lance une requête GET en accord avec la définition de l'API
             et retourne le résultat ou lance une erreur le cas échéant.
         """
+        if payload is None:
+            payload = {}
+
         for i, api in enumerate(self.API_list, start=1):
             self.selected_API = api
 
@@ -135,7 +146,7 @@ class API_Manager:
                     return self.get_name(), res.json()
                 elif i == len(self.API_list):
                     return None
-            except Exception:
+            except ValueError:
                 if i <= len(self.API_list) - 1:
                     continue
                 else:
